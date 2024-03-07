2deba4d6e08a9ab4f4fef624fbde2cc6f2f7d6b1
Lucy <124856770+gjiyun@users.noreply.github.com>
2024-03-06 18:54:15 -0700

    TDD hasCity no implementation

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,10 +1,13 @@
 package com.example.lab8;
 
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +23,6 @@ public class CustomListTest {
         list = new CustomList(null,new ArrayList<>());
         return list;
     }
-
     /**
      * get the size of the list
      * increase the list by adding a new city
@@ -33,6 +35,17 @@ public class CustomListTest {
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
     }
-
-
+    /**
+     * check if city is contained in list
+     * add city to list
+     * check again if city is now contained in list
+     */
+    @Test
+    void testHasCity(){
+        list = MockCityList();
+        City city = new City("Vancouver","British Columbia");
+        assertFalse(list.hasCity(city));
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+    }
 }
